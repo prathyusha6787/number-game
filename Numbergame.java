d0c69e0bac31b58d9dab160d43afa408ee1cc616
prathyusha6787 <154258928+prathyusha6787@users.noreply.github.com>
2023-12-18 23:36:45 +0530

    Add files via upload

---
+++ Numbergame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.numbergame;
+
+/**
+ *
+ * @author DELL 3400
+ */
+public class Numbergame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
